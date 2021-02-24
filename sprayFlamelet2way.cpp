287e2da23bc11d646bc038497f758be67924420e
yujietang <yujietang1997@outlook.com>
2021-02-24 15:41:25 +0800

    Update the main program

--- sprayFlamelet2way.cpp
+++ sprayFlamelet2way.cpp
@@ -15,25 +15,27 @@ int main()
     /************************************************************/
     //                       Injection
     /************************************************************/
-    doublereal parcelDiameter(60e-6); // m
-    doublereal injTemperature(300); // K
-    doublereal injPressure(1.0*OneAtm); // Pa
-    doublereal dtlag = 10e-6;
+    bool do_spray = false; // do_spray = 1: two-way couple; do_spray = 0: pure gas;
+    doublereal parcelDiameter(60e-6); // injection droplet diameter [m]
+    doublereal injTemperature(300); // injection parcel's temperature [K]
+    doublereal injPressure(1.0*OneAtm); // injection pressure [Pa]
+    doublereal dtlag = 10e-6; // Parcel Tracking Time Scale(Evaporation Time Scale)
     doublereal dropletNumber;
     /************************************************************/
     //                          Mesh
     /************************************************************/
-    const size_t meshPointNumber = 10;
-    const doublereal domainLength = 0.1;
-    doublereal minGrid = parcelDiameter;//min grid size.
+    const size_t meshPointNumber = 10; // Mesh Point Number
+    const doublereal domainLength = 0.1; // Domain Length
+    doublereal minGrid = parcelDiameter;// min grid size.
+    bool refine_grid = false; // Refined mesh has been turned off
     /************************************************************/
     //                        Gas Flow
     /************************************************************/
-    const doublereal temp = 300.0; // K
-    const doublereal pressure = 1.0*OneAtm; //atm
-    doublereal uin = 0.3; //m/sec
-    const doublereal phi_over = 1.0;//overall equivalence ratio
-    const doublereal phi = phi_over; //equivalence ratio
+    const doublereal temp = 300.0; // inlet gas flow temperature [K]
+    const doublereal pressure = 1.0*OneAtm; // inlet gas flow pressure [atm]
+    doublereal uin = 0.3; // initial guess of inlet velocity [m/s]
+    const doublereal phi_over = 1.0;// overall equivalence ratio
+    const doublereal phi = phi_over; // gas flow equivalence ratio
     /************************************************************/
     //                        Chemistry
     /************************************************************/
@@ -41,12 +43,12 @@ int main()
     doublereal C_atoms = 2.0; // C2H5OH
     doublereal H_atoms = 6.0; // C2H5OH
     doublereal O_atoms = 1.0; // C2H5OH
-    size_t nsp = gas.nSpecies();
+    size_t nsp = gas.nSpecies(); // number of species
     vector_fp x(nsp, 0.0);
     doublereal ax = C_atoms + H_atoms/4.0 - O_atoms/2.0; // air consumption
     doublereal fa_stoic = 1.0 / (4.76 * ax); // fuel / air ratio at stoic state  
-    // when the condition is fuel/oxidizer flow:
-    x[gas.speciesIndex("C2H5OH")] = 1.0;
+    // Species' mole fraction when the condition is fuel/oxidizer flow:
+    x[gas.speciesIndex("C2H5OH")] = 1.0; 
     x[gas.speciesIndex("O2")] = 0.21 / phi / fa_stoic;
     x[gas.speciesIndex("N2")] = 0.78 / phi / fa_stoic;
     x[gas.speciesIndex("AR")] = 0.01 / phi / fa_stoic;
@@ -154,9 +156,6 @@ int main()
     /**********Solve freely propagating flame with spray cloud**********/
     sprayflame.setFixedTemperature(0.5 * (temp + Tad));
     gasflow.solveEnergyEqn();
-    bool refine_grid = false;
-    // bool do_spray = true;
-    bool do_spray = false;
     sprayflame.solve(loglevel, refine_grid, do_spray); 
 
     double flameSpeed_mix = sprayflame.value(flowdomain,gasflow.componentIndex("u"),0);
